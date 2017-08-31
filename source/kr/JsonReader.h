70d6f1691b32cc046929803c243658b4a5565ba9
kracejic <kracejic@gmail.com>
2017-08-31 16:22:16 -0500

    JsonReader - arrays works

--- source/kr/JsonReader.h
+++ source/kr/JsonReader.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+#include <iostream>
+
 namespace kr
 {
 
@@ -13,11 +15,13 @@ namespace Json
     {
         enum class Type
         {
+            NA,
             object,
             array,
             string,
             number,
-            bolean,
+            true_,
+            false_,
             null,
             keyval
         };
@@ -25,57 +29,154 @@ namespace Json
         class TreeNode
         {
           public:
-            TreeNode(Type _type, size_t start)
+            TreeNode(Type _type, char* start)
                 : type(_type)
                 , text_start(start){};
+            TreeNode(Type _type, char* start, char* end)
+                : type(_type)
+                , text_start(start)
+                , text_end(end){};
             Type type{Type::null};
             // indexes ins
-            size_t text_start{0};
-            size_t text_end{0};
+            char* text_start{0};
+            char* text_end{0};
 
-            int tree_end{0};
+            int size{1};
         };
 
         class Json
         {
-            TreeNode* me;
+          public:
+            TreeNode* me{nullptr};
+            Json(){};
+            Json(TreeNode* _it)
+                : me(_it){};
+            Json& operator++()
+            {
+                me = me + me->size;
+                return *this;
+            }
+            Json& operator*()
+            {
+                return *this;
+            }
+            bool operator!=(const Json& rhs)
+            {
+                return me != rhs.me;
+            }
+
+            bool operator!=(std::nullptr_t p)
+            {
+                (void)p;
+                return not isNull();
+            }
+            bool operator==(std::nullptr_t p)
+            {
+                (void)p;
+                return isNull();
+            }
+
+            Json operator[](size_t index)
+            {
+                Json ret = this->begin();
+                for (size_t i = 0; i < index; ++i)
+                    ++ret;
+                return ret;
+            }
+            size_t size()
+            {
+                Json ret = this->begin();
+                size_t count = 0;
+                while (ret != this->end())
+                {
+                    ++count;
+                    ++ret;
+                }
+                return count;
+            }
+
+            Json begin()
+            {
+                return {me + 1};
+            }
+            Json end()
+            {
+                return {me + me->size};
+            }
+            std::string str()
+            {
+                return {me->text_start, me->text_end};
+            }
+
+
+
+            // clang-format off
+            operator std::string() { return {me->text_start, me->text_end}; }
+            operator bool() { return me->type == _detail::Type::true_; }
+            operator int() { return strtol(me->text_start, nullptr, 10); }
+            operator long() { return strtol(me->text_start, nullptr, 10); }
+            operator float() { return strtof(me->text_start, nullptr); }
+            operator double() { return strtod(me->text_start, nullptr); }
+
+            bool operator==(const std::string& rhs) { return operator std::string() == rhs; }
+            bool operator==(const char* rhs) { return operator std::string() == rhs; }
+            bool operator==(long rhs) { return operator long() == rhs; }
+            bool operator==(int rhs) { return operator int() == rhs; }
+            bool operator==(float rhs) { return operator float() == rhs; }
+            bool operator==(double rhs) { return operator double() == rhs; }
+            bool operator==(bool rhs) { return operator bool() == rhs; }
+
+            bool operator!=(const std::string& rhs) { return operator std::string() != rhs; }
+            bool operator!=(const char* rhs) { return operator std::string() != rhs; }
+            bool operator!=(long rhs) { return operator long() != rhs; }
+            bool operator!=(int rhs) { return operator int() != rhs; }
+            bool operator!=(float rhs) { return operator float() != rhs; }
+            bool operator!=(double rhs) { return operator double() != rhs; }
+            bool operator!=(bool rhs) { return operator bool() != rhs; }
+
+            bool isNull() { return me->type == _detail::Type::null; };
+            bool isNumber() { return me->type == _detail::Type::number; };
+            bool isString() { return me->type == _detail::Type::string; };
+            bool isObject() { return me->type == _detail::Type::object; };
+            bool isArray() { return me->type == _detail::Type::array; };
+            // clang-format on
         };
     }
 
-    class Array
+    class Array : public _detail::Json
     {
     };
-    class Object
+    class Object : public _detail::Json
     {
     };
 
-    class Reader
+    class Reader : public _detail::Json
     {
       private:
+      public:
         std::string text;
         std::vector<_detail::TreeNode> tree;
 
         void _parse()
         {
             std::vector<size_t> st;
-            bool inText = false;
-            bool inNumber = false;
+            st.emplace_back(0);
+
             for (size_t i = 0; i < text.size(); ++i)
             {
                 char& ch = text[i];
-                if (inText && ch != '"')
-                    continue;
-                if (inNumber && (std::isdigit(ch) || ch == '.'))
-                    continue;
 
                 switch (ch)
                 {
                     case '"':
-                        if (not inText)
-                            tree.emplace_back(_detail::Type::string, i + 1);
-                        else
-                            tree.back().text_end = i - 1;
-                        inText = !inText;
+                        std::cout << "TEXT\n";
+                        tree.emplace_back(_detail::Type::string, &text[i + 1]);
+                        do
+                        {
+                            ++i;
+                            std::cout << " text[i] = " << text[i] << "\n";
+                        } while (text[i] != '"');
+                        tree.back().text_end = &text[i];
                         break;
 
                     case '0':
@@ -91,29 +192,75 @@ namespace Json
                     case '-':
                     case '+':
                     case '.':
-                        inNumber = true;
-                        tree.emplace_back(_detail::Type::number, i);
+                        std::cout << "NUMBER\n";
+                        tree.emplace_back(_detail::Type::number, &text[i]);
+                        do
+                        {
+                            ++i;
+                            std::cout << " text[i] = " << text[i] << "\n";
+                        } while (std::isdigit(text[i]) || text[i] == '.');
+                        tree.back().text_end = &text[i];
+                        --i;
+                        break;
+
+                    case 'f':
+                    case 'F':
+                        if (text[i + 1] == 'a' && text[i + 2] == 'l' &&
+                            text[i + 3] == 's' && text[i + 4] == 'e')
+                        {
+                            std::cout << "FALSE\n";
+                            tree.emplace_back(
+                                _detail::Type::false_, &text[i], &text[i + 5]);
+                            i += 4;
+                        }
+                        break;
+                    case 't':
+                    case 'T':
+                        if (text[i + 1] == 'r' && text[i + 2] == 'u' &&
+                            text[i + 3] == 'e')
+                        {
+                            std::cout << "TRUE\n";
+                            tree.emplace_back(
+                                _detail::Type::true_, &text[i], &text[i + 4]);
+                            i += 3;
+                        }
+                        break;
+                    case 'n':
+                        if (text[i + 1] == 'u' && text[i + 2] == 'l' &&
+                            text[i + 3] == 'l')
+                        {
+                            std::cout << "NULL\n";
+                            tree.emplace_back(
+                                _detail::Type::null, &text[i], &text[i + 4]);
+                            i += 3;
+                        }
                         break;
 
                     case '[':
+                        std::cout << "ARRAY\n";
                         st.emplace_back(tree.size());
-                        tree.emplace_back(_detail::Type::array, i);
+                        tree.emplace_back(_detail::Type::array, &text[i]);
                         break;
                     case ']':
-                        tree[st.back()].text_end = i;
+                        std::cout << "st.back() = " << st.back() << "\n";
+                        tree[st.back()].text_end = &text[i + 1];
+                        tree[st.back()].size = tree.size() - st.back();
                         st.pop_back();
                         break;
                     case '{':
+                        std::cout << "OBJECT\n";
                         st.emplace_back(tree.size());
-                        tree.emplace_back(_detail::Type::object, i);
+                        tree.emplace_back(_detail::Type::object, &text[i]);
                         break;
                     case '}':
-                        tree[st.back()].text_end = i;
+                        tree[st.back()].text_end = &text[i + 1];
+                        tree[st.back()].size = tree.size() - st.back();
                         st.pop_back();
                         break;
                     default: break;
                 }
             }
+            me = &(tree.front());
         }
 
       public:
@@ -127,30 +274,6 @@ namespace Json
         {
             _parse();
         }
-        bool isNull()
-        {
-            return tree.front().type == _detail::Type::null;
-        };
-        bool isBool()
-        {
-            return tree.front().type == _detail::Type::bolean;
-        };
-        bool isNumber()
-        {
-            return tree.front().type == _detail::Type::number;
-        };
-        bool isString()
-        {
-            return tree.front().type == _detail::Type::string;
-        };
-        bool isObject()
-        {
-            return tree.front().type == _detail::Type::object;
-        };
-        bool isArray()
-        {
-            return tree.front().type == _detail::Type::array;
-        };
         // TODO more
 
         void parse(std::string&& input)
--- source/unittest/jsonReader.cpp
+++ source/unittest/jsonReader.cpp
@@ -1,6 +1,9 @@
 #include "catch.hpp"
 #include "kr/JsonReader.h"
 
+#include <iostream>
+
+
 using namespace kr::Json;
 TEST_CASE("JsonReader simple jsons")
 {
@@ -18,7 +21,30 @@ TEST_CASE("JsonReader simple jsons")
 }
 
 
+TEST_CASE("JsonReader simple array")
+{
+    std::cout << "--------------- \n";
+    Reader r{R"([85, "xx", ["aa" ,1, true, null,false ], -13, 3.6])"};
 
+    REQUIRE(r.isArray());
+    REQUIRE(r.size() == 5);
+    REQUIRE(r[1].size() == 0);
+    REQUIRE(r[2].size() == 5);
 
+    for (auto& e : r)
+    {
+        REQUIRE(not e.isObject());
+    }
+
+    REQUIRE(r[0] == 85);
+    REQUIRE(r[1] == "xx");
+    REQUIRE(r[2][0] == "aa");
+    REQUIRE(r[2][1] == 1);
+    REQUIRE(r[2][2] == true);
+    REQUIRE(r[2][3] == nullptr);
+    REQUIRE(r[2][4] == false);
+    REQUIRE(r[3] == -13);
+    REQUIRE(r[4] == 3.6);
+}
 
 
